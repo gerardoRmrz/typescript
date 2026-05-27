21c9b03720f2a1350dda7da1ef684cc49e29db45
gerardoRmrz <grmzrso@outlook.com>
2026-05-26 20:07:54 -0500

    The bmiCalculator.ts script has been created

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,58 @@
+interface InputValues {
+  value1: number;
+  value2: number;
+}
+
+const parseArguments = (args: string[]): InputValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (Number(args[2]) === 0)
+    throw new Error("Height must be greater than zero");
+
+  if (Number(args[2]) < 0 || Number(args[3]) < 0)
+    throw new Error("Arguments must be positive");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values are not numbers!");
+  }
+};
+
+const bmiCalculator = (height: number, weight: number): string => {
+  const bmi: number = Number((weight / (height / 100) ** 2).toFixed(2));
+  if (bmi < 16) {
+    return `Underweight (Severe thinness)`;
+  } else if (bmi >= 16 && bmi < 17) {
+    return `Underweight (Moderate thinness)`;
+  } else if (bmi >= 17 && bmi < 18.5) {
+    return `Underweight (Mid thinness)`;
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return `Normal range`;
+  } else if (bmi >= 25 && bmi < 30) {
+    return `Overweight (Pre-obese)`;
+  } else if (bmi >= 30 && bmi < 35) {
+    return `Obese (Class I)`;
+  } else if (bmi >= 35 && bmi < 40) {
+    return `Obese (Class II)`;
+  } else if (bmi >= 40) {
+    return `Obese (Class III)`;
+  }
+
+  return "Values out of range";
+};
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  console.log(bmiCalculator(value1, value2));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += "Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
